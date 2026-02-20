5738da4b9b14249ab6f8d4e69c12ce8dbe1393e7
Heinz <ateng.heinz@gmail.com>
2026-02-20 12:29:57 +0300

    6

--- database/migrations/2026_02_20_091250_alter_table_users_add_role_id.php
+++ database/migrations/2026_02_20_091250_alter_table_users_add_role_id.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            
+            $table->boolean('is_active');
+            $table->string('user_image')->nullable();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
